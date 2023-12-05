c50a5928dcc3c5db4e8580f085eca3cc428c2d55
diamondburned <diamond@arikawa-hi.me>
2023-12-05 03:08:38 -0800

    Add helper functions and classes for ai.py

--- ai.py
+++ ai.py
@@ -6,8 +6,9 @@ import hashlib
 import base64
 import openai
 import openai.types
+import asyncio
 import dbm.dumb as db
-from typing import MutableMapping, Generator
+from typing import Any, Callable, MutableMapping, Generator, TypeVar
 from resume import *
 from dotenv import load_dotenv
 
@@ -39,7 +40,10 @@ def hash_input(input: str) -> str:
     return base64.b64encode(hash.digest()).decode("utf-8")
 
 
-async def get_embeddings(inputs: list[str], cache=True) -> list[list[float]]:
+Embeddings = list[list[float]]
+
+
+async def get_embeddings(inputs: list[str], cache=True) -> Embeddings:
     # sanitize whitespace
     inputs = [input.replace("\n", " ").strip() for input in inputs]
 
@@ -80,7 +84,17 @@ async def get_embeddings(inputs: list[str], cache=True) -> list[list[float]]:
     return embeddings
 
 
-async def search_embeddings(query: str, embeddings: list[list[float]]) -> list[int]:
+ListItemT = TypeVar("ListItemT")
+
+
+async def get_list_embeddings(
+    inputs: list[ListItemT],
+    mapper: Callable[[ListItemT], str],
+) -> Embeddings:
+    return await get_embeddings([mapper(input) for input in inputs])
+
+
+async def search_embeddings(query: str, embeddings: Embeddings) -> list[int]:
     """
     Search the list of embeddings for the closest match to the query.
     It returns a list of indices, sorted by closeness to the query.
@@ -93,17 +107,29 @@ async def search_embeddings(query: str, embeddings: list[list[float]]) -> list[i
     )
 
 
+async def search_list(
+    inputs: list[ListItemT],
+    mapper: Callable[[ListItemT], str],
+    query: str,
+) -> list[ListItemT]:
+    """
+    Search the list of inputs for the closest match to the query.
+    It returns a list of inputs, sorted by closeness to the query.
+    """
+    embeddings = await get_list_embeddings(inputs, mapper)
+    result = await search_embeddings(query, embeddings)
+    return [inputs[i] for i in result]
+
+
 async def resume_embeddings(resume: Resume):
     assert len(resume.work) < 20
     assert len(resume.projects) < 20
     assert len(resume.education) < 10
     assert len(resume.awards) < 10
 
-    work_embeddings = await get_embeddings(
-        [
-            f"{work.position} at {work.company}.\n{' '.join(work.highlights)}"
-            for work in resume.work
-        ]
+    work_embeddings = await get_list_embeddings(
+        resume.work,
+        lambda w: f"{w.position} at {w.company}.\n{' '.join(w.highlights or [])}",
     )
     print(len(work_embeddings), [len(embedding) for embedding in work_embeddings])
 
@@ -117,3 +143,78 @@ async def resume_embeddings(resume: Resume):
 
     result = [resume.work[i] for i in result]
     print([f"{work.position} at {work.company}" for work in result])
+
+
+class ResumeSorter:
+    """
+    Sorts a resume by relevance to a search query.
+    It automatically uses the OpenAI Embeddings API to generate embeddings and
+    cache them for future use.
+    """
+
+    search: str
+    _resume: Resume
+
+    def __init__(self, resume: Resume, search: str = ""):
+        assert len(resume.work) < 20
+        assert len(resume.projects) < 20
+        assert len(resume.education) < 10
+        assert len(resume.awards) < 10
+
+        self.search = search
+        self.resume = resume
+
+    @property
+    def resume(self) -> Resume:
+        return self._resume
+
+    @resume.setter
+    def resume(self, resume: Resume):
+        self._resume = resume.model_copy(deep=True)
+
+    async def sort(
+        self,
+        work=True,
+        projects=True,
+        education=True,
+        awards=True,
+    ):
+        coros = []
+        if work:
+            coros.append(self._sort_work())
+        if projects:
+            coros.append(self._sort_projects())
+        if education:
+            coros.append(self._sort_education())
+        if awards:
+            coros.append(self._sort_awards())
+
+        await asyncio.gather(*coros)
+
+    async def _sort_work(self):
+        self._resume.work = await search_list(
+            self._resume.work,
+            lambda w: f"{w.position} at {w.company}.\n{' '.join(w.highlights or [])}",
+            self.search,
+        )
+
+    async def _sort_projects(self):
+        self._resume.projects = await search_list(
+            self._resume.projects,
+            lambda p: f"{p.name}: \n{p.description} ({' '.join(p.keywords or [])})",
+            self.search,
+        )
+
+    async def _sort_education(self):
+        self._resume.education = await search_list(
+            self._resume.education,
+            lambda e: f"{e.institution} ({e.studyType}, {e.area}): {e.startDate} - {e.endDate}",
+            self.search,
+        )
+
+    async def _sort_awards(self):
+        self._resume.awards = await search_list(
+            self._resume.awards,
+            lambda a: f"{a.title}: {a.summary} ({a.date})",
+            self.search,
+        )
