8f5b686eeb41878d937c3a3baa61cd6bce3b397e
diamondburned <diamond@arikawa-hi.me>
2023-12-05 03:09:12 -0800

    Implement mini REST API
    Closes #4

--- server.py
+++ server.py
@@ -1,16 +1,53 @@
 #!/usr/bin/env python3
 
 import uvicorn
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, APIRouter, Query, Response, Body
+from fastapi.staticfiles import StaticFiles
+from resume import *
+from ai import ResumeSorter
+
+api = APIRouter(prefix="/api")
+
+
+class SortResumeResponse(BaseModel):
+    resume: Resume
+
+
+@api.post("/resume/sort")
+async def sort_resume(
+    query: str,
+    work=True,
+    projects=True,
+    education=True,
+    awards=True,
+    resume: Resume = Body(),
+) -> SortResumeResponse:
+    """
+    Sorts a resume by relevance to a search query.
+    It automatically uses the OpenAI Embeddings API to generate embeddings and
+    cache them for future use.
+    """
+
+    sorter = ResumeSorter(resume, query)
+    await sorter.sort(work, projects, education, awards)
+    return SortResumeResponse(resume=sorter.resume)
+
+
+@api.post("/resume.pdf")
+async def resume_pdf(resume: Resume = Body()) -> Response:
+    """
+    Generates a PDF of the resume.
+    """
+    print(resume.dict())
+    pdf_data = await generate_pdf(resume)
+    return Response(content=pdf_data, media_type="application/pdf")
 
 
 app = FastAPI()
-
-
-@app.get("/")
-async def index():
-    return {"Hello": "World"}
+app.include_router(api)
+app.mount("/", StaticFiles(directory="frontend", html=True))
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=9000, reload=True)
